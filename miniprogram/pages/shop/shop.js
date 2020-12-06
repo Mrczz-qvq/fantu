770f69cb3f6a0d462c2d3e307740f264d6b3a41d
暴风磁感线 <70740571+magnetic233@users.noreply.github.com>
2020-12-06 21:49:04 +0800

    完成了上传功能 通过引入餐厅序号 解决了shop页面获取商品列表时 有店名重复的问题 另:更新了玫瑰二楼地图
    上传菜品若留空数据则无法上传  shop页面查询双条件：名字和食堂序号   接来下等待其他功能和大量！！！食堂数据

--- miniprogram/pages/shop/shop.js
+++ miniprogram/pages/shop/shop.js
@@ -17,7 +17,8 @@ Page({
     ]
   },
 gotocanteen:function(event){
-  console.log('传参到餐厅页面的参数:',event.currentTarget.dataset.text)
+  console.log('传参到餐厅页面的参数 店名:',event.currentTarget.dataset.text)
+  console.log('传参到餐厅页面的参数 食堂序号:',this.data.canteennum)
   wx.navigateTo({
       url: '../../pages/canteen/canteen?highlight='+event.currentTarget.dataset.text+'&canteennum='+this.data.canteennum // 传递高亮的店名称和所在食堂的序号
     })
@@ -29,37 +30,43 @@ gotocanteen:function(event){
     var that = this
     if(options.Shop)
     {
-      console.log('获取当前超市名称：shop参数:',options.Shop,'获取当前食堂序号：canteennum参数:',options.canteennum)
+      console.log('获取当前超市名称：shop参数:',options.Shop,'获取当前食堂序号canteennum:',options.canteennum)
       that.setData({
         shopname:options.Shop,
         canteennum:options.canteennum  
       })//加载时务必保证每个页面到shop页面的参数成功传了 到shop页面途径:食堂点击√+刷一刷点击（等待后台数据ing）
-    }
-    db.collection('dishes').where({
-      'store':_.eq(options.Shop)
-    })
-    .get({
-      success: function(res) {
-        // res.data 是包含以上定义的一条记录的数组
-        console.log("在dishes中搜索到商品：",res.data)
-        that.setData({
-          goods:res.data//返回改菜品的_id（目前只能返回一个）
-        })
-      }
-    })
 
-    db.collection('store'+options.canteennum).where({
-      'name':_.eq(options.Shop)
-    })
-    .get({
-      success: function(res) {
-        // res.data 是包含以上定义的一条记录的数组
-        console.log("在",'store'+options.canteennum,'搜索到该商店：',res.data[0].name)
-        that.setData({
-          img_src:res.data[0]['storeImg']//返回改菜品的_id（目前只能返回一个）
-        })
-      }
-    })
+      db.collection('store'+options.canteennum).where({
+        'name':_.eq(options.Shop)
+      })
+      .get({
+        success: function(res) {
+          // res.data 是包含以上定义的一条记录的数组
+          console.log("在",'store'+options.canteennum,'搜索到该商店：',res.data[0].name)
+          that.setData({
+            img_src:res.data[0]['storeImg']//返回对应store的图片路径（目前只能返回一个）
+          })
+        }
+      })
+  
+      db.collection('dishes').where({
+        store:options.Shop,
+        canteennum:parseInt(options.canteennum),
+      })
+      .get({
+        success: function(res) {
+          // res.data 是包含以上定义的一条记录的数组
+          console.log("在dishes中搜索到商品：",res.data)
+          that.setData({
+            goods:res.data//返回改菜品的_id（目前只能返回一个）
+          })
+        }
+      })
+    }
+
+    
+
+
   },
 
   collect: function(){
